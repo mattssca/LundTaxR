81f4c7257d438eead60d2858033995a635cc638c
mattssca <mattsada@gmail.com>
2025-10-16 08:22:59 +0200

    removing site build script

--- site_build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-git checkout pkgdown
-git merge main
-Rscript -e "pkgdown::build_site()"
-git add .
-git commit -m "Auto-update pkgdown site $(date)"
-git push origin pkgdown
-echo "Site updated and deployed!"
